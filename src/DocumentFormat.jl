36d0dec95d5d8716968e572e89c8f2202ac56058
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-31 11:04:33 +0000

    Snapshot of branch master taken on 2019-07-31T07:04:32.549-04:00 from "https://github.com/UnofficialJuliaMirror/DocumentFormat.jl-ffa9a821-9c82-50df-894e-fbcef3ed31cd"

--- src/DocumentFormat.jl
+++ src/DocumentFormat.jl
@@ -1,11 +1,21 @@
 module DocumentFormat
 using CSTParser
+using CSTParser.Tokenize
 import CSTParser.Tokenize.Tokens
 
 mutable struct FormatOptions
     indent::Int
+    indents::Bool
+    ops::Bool
+    tuples::Bool
+    curly::Bool
+    calls::Bool
+    iterOps::Bool
+    comments::Bool
+    docs::Bool
+    lineends::Bool
 end
-FormatOptions() = FormatOptions(4)
+FormatOptions() = FormatOptions(4, true, true, true, true, true, true, true, true, true)
 
 struct Edit{T}
     loc::T
@@ -18,30 +28,48 @@ mutable struct State{T}
     opts::FormatOptions
 end
 
-function format(text; convert_iterator_ops = false)
-    original_ast = CSTParser.remlineinfo!(Meta.parse(string("begin\n",text, "\nend")))
-    state = State(0, Edit[], FormatOptions())
+function format(text, formatopts::FormatOptions = FormatOptions())
+    original_ast = CSTParser.remlineinfo!(Meta.parse(string("begin\n", text, "\nend")))
+    state = State(0, Edit[], formatopts)
     x = CSTParser.parse(text, true)
-    pass(x, state, operator_pass)
-    state.offset = 0
-    pass(x, state, tuple_pass)
-    state.offset = 0
-    pass(x, state, curly_pass)
-    state.offset = 0
-    pass(x, state, call_pass)
-    if convert_iterator_ops
+    if formatopts.ops
+        pass(x, state, operator_pass)
+    end
+    if formatopts.tuples
+        state.offset = 0
+        pass(x, state, tuple_pass)
+    end
+    if formatopts.curly
+        state.offset = 0
+        pass(x, state, curly_pass)
+    end
+    if formatopts.calls
+        state.offset = 0
+        pass(x, state, call_pass)
+    end
+    if formatopts.iterOps
         state.offset = 0
         pass(x, state, forloop_pass)
     end
-    state.offset = 0
-    pass(x, state, doc_pass)
+    if formatopts.comments
+        comments_pass(text, state)
+    end
+    if formatopts.docs
+        state.offset = 0
+        pass(x, state, doc_pass)
+    end
+    if formatopts.lineends
+        lineends_pass(text, state)
+    end
     sort!(state.edits, lt = (a, b)->first(a.loc) < first(b.loc), rev = true)
 
     for i = 1:length(state.edits)
         text = apply(text, state.edits[i])
     end
-    text = indents(text, state.opts)
-    new_ast = CSTParser.remlineinfo!(Meta.parse(string("begin\n",text, "\nend")))
+    if formatopts.indents
+        text = indents(text, state.opts)
+    end
+    new_ast = CSTParser.remlineinfo!(Meta.parse(string("begin\n", text, "\nend")))
     original_ast != new_ast && error("Mismatch between AST of original and formatted text")
     return text
 end
@@ -61,7 +89,7 @@ end
 function ensure_single_space_after(x, state, offset)
     if x.fullspan == x.span
         push!(state.edits, Edit(offset + x.fullspan, " "))
-        
+
     end
 end
 
@@ -77,7 +105,7 @@ function apply(text, edit::Edit{Int})
 end
 function apply(text, edit::Edit{UnitRange{Int}})
     v = Vector{UInt8}(deepcopy(text))
-    String(vcat(v[1:first(edit.loc)-1], Vector{UInt8}(edit.text), v[last(edit.loc) + 1: end]))
+    String(vcat(v[1:first(edit.loc) - 1], Vector{UInt8}(edit.text), v[last(edit.loc) + 1:end]))
 end
 include("passes.jl")
 include("indents.jl")
